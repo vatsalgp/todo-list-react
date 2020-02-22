10014c49cefc057891f6db6936887e73bea6fda9
vatsalgp <vatsalgupta280@gmail.com>
2020-02-23 00:01:52 +0530

    Removed Redux-dev-tools

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDom from "react-dom";
 import reduxThunk from "redux-thunk";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+// import { compose } from "redux";
 
 import App from "./Components/App";
 import reducers from "./Reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = x => x;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDom.render(
