5c43e09d9601aff661863b30326a37b20b701ca1
vatsalgp <vatsalgupta280@gmail.com>
2020-04-16 23:41:08 +0530

    changed gitignore

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZP0jrXVhmIcUwexFbrpTvpkFdIwD153c",
+    authDomain: "todo-list-3410d.firebaseapp.com",
+    databaseURL: "https://todo-list-3410d.firebaseio.com",
+    projectId: "todo-list-3410d",
+    storageBucket: "todo-list-3410d.appspot.com",
+    messagingSenderId: "642124068599",
+    appId: "1:642124068599:web:0154e1991a613051d6c5f2"
+};
+
+export default firebaseConfig;
